24148d483624847518299c64c5134a12785db34a
Harry Ross <hazross@hotmail.com>
2020-01-25 00:56:56 +1100

    added google maps and basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +9,91 @@ class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
-            title: 'Welcome to Flutter',
-            home: Scaffold(
-                appBar: AppBar(
-                    title: Text('Welcome to Flutter'),
-                ),
-                body: Center(
-                    child: Text('Hello World'),
-                ),
+            title: "Haha epic",
+            home: MyStatefulWidget(),
+        );
+    }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+    MyStatefulWidget({Key key}) : super(key: key);
+
+    @override
+    _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+    // TODO -This is really messily done, redo later
+    static Completer<GoogleMapController> _controller = Completer();
+
+    static const LatLng _center = const LatLng(-33.782583, 151.239167);
+
+    void _onMapCreated(GoogleMapController controller) {
+        _controller.complete(controller);
+    }
+
+    MapType _currentMapType = MapType.normal;
+
+    int _selectedIndex = 0;
+    static const TextStyle optionStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+    List<Widget> _widgetOptions = <Widget>[
+        GoogleMap(
+          onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+            },
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+        Text(
+            'Leaderboard here',
+            style: optionStyle,
+        ),
+        Text(
+            'Something important here',
+            style: optionStyle,
+        ),
+    ];
+
+    void _onItemTapped(int index) {
+        setState(() {
+            _selectedIndex = index;
+        });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text("Gamer time"),
+            ),
+            body: Center(
+                child: _widgetOptions.elementAt(_selectedIndex),
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        title: Text('Home'),
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        title: Text('Home'),
+                    ),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        title: Text('Home'),
+                    ),
+                ],
+                currentIndex: _selectedIndex,
+                selectedItemColor: Colors.amber[800],
+                onTap: _onItemTapped,
             ),
         );
+ 
     }
 }
