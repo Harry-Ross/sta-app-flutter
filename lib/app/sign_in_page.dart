2a38e7f6f9eb12dbc2fd5fd8395f3ad323d5822b
Harry Ross <hazross@hotmail.com>
2020-01-31 23:42:52 +1100

    did some code

--- lib/app/sign_in_page.dart
+++ lib/app/sign_in_page.dart
@@ -2,14 +2,24 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:sta_app/app/authentication_state.dart';
+import 'package:sta_app/app/services/authentication_service.dart';
 
 class SignInPage extends StatelessWidget {
     final StreamController<AuthenticationState> _streamController;
+    AuthenticationService _authenticationService = new AuthenticationService();
 
     SignInPage(this._streamController);
 
     signIn() async {
         _streamController.add(AuthenticationState.authenticated());
+        var result = await _authenticationService.authenticate("username", "password");
+
+        if (result) {
+            _streamController.add(AuthenticationState.authenticated());
+        }
+        else {
+            _streamController.add(AuthenticationState.failed());
+        }
     }
 
     Widget build(BuildContext context) {
