0114f23dff5c6555835bf14c7cee53fd81f7398f
Harry Ross <hazross@hotmail.com>
2020-02-07 23:09:40 +1100

    fixed something

--- lib/screens/home/widgets/endpoint_card.dart
+++ lib/screens/home/widgets/endpoint_card.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class EndpointCard extends StatelessWidget {
+
+    final Map<String, Marker> locations = { 
+        "firstOne": Marker(
+            markerId: MarkerId("Yes that is quite epic time"),
+            position: LatLng(-33.800243, 151.283743),
+            infoWindow: InfoWindow(title: "yes"),
+        ),
+    };
+
     Widget build(BuildContext context) {
         return Card(
             child: Column(
@@ -16,7 +25,7 @@ class EndpointCard extends StatelessWidget {
                     ),
                     Container(
                         padding: const EdgeInsets.all(10),
-                        //alignment: Alignment.centerLeft,
+                        alignment: Alignment.centerLeft,
                         child: RichText(
                             text: TextSpan(
                                 style: new TextStyle(
@@ -35,9 +44,10 @@ class EndpointCard extends StatelessWidget {
                         padding: const EdgeInsets.all(10),
                         height: 150,
                         child: GoogleMap(
+                            markers: locations.values.toSet(),
                             initialCameraPosition: CameraPosition(
-                                target: LatLng(-33.782583, 151.239167),
-                                zoom: 11.0,
+                                target: LatLng(-33.800243, 151.283743),
+                                zoom: 9.0,
                             ),
                         )
                     )
